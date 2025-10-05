2b7b5f0a589ec735dbb097c687ee699a51ce698a
Ruben Escribano <rubenescribanomendiola@gmail.com>
2025-10-05 20:59:11 +0200

    PArámetro de ordenación

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import sys
 
 DEFAULT_FILENAME = "words.txt"
 DEFAULT_DUPLICATES = False
+DEFAULT_ASCENDING = True
 
 
 def sort_list(items, ascending=True):
@@ -24,16 +25,24 @@ def remove_duplicates_from_list(items):
 if __name__ == "__main__":
     filename = DEFAULT_FILENAME
     remove_duplicates = DEFAULT_DUPLICATES
-    if len(sys.argv) == 3:
+    ascending = DEFAULT_ASCENDING
+
+    if len(sys.argv) == 4:
+        filename = sys.argv[1]
+        remove_duplicates = sys.argv[2].lower() == "yes"
+        ascending = sys.argv[3].lower() == "asc"
+    elif len(sys.argv) == 3:
         filename = sys.argv[1]
         remove_duplicates = sys.argv[2].lower() == "yes"
     else:
         print("Se debe indicar el fichero como primer argumento")
         print("El segundo argumento indica si se quieren eliminar duplicados")
+        print("Un tercer argumento opcional: <yes/no> para ascendente/descendente")
         sys.exit(1)
 
     print(f"Se leerán las palabras del fichero {filename}")
     file_path = os.path.join(".", filename)
+
     if os.path.isfile(file_path):
         word_list = []
         with open(file_path, "r") as file:
@@ -46,4 +55,4 @@ if __name__ == "__main__":
     if remove_duplicates:
         word_list = remove_duplicates_from_list(word_list)
 
-    print(sort_list(word_list))
+    print(sort_list(word_list, ascending=ascending))
